e579a8a3d804cdf4ad0a2f8a59ad0915a38581a1
dodeca-dev <jrussclay@gmail.com>
2020-02-23 18:48:25 +0800

    add infinite scroll

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,35 +11,82 @@ import Categories from '../components/Home/Categories';
 import axios from 'axios';
 import apiURL from '../utils/apiURL';
 import SkeletonGrid from '../components/Shared/Loader/SkeletonGrid';
+import colors from '../utils/colors';
 
 const Home = ({ navigation }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasLoadMore, setHasLoadMore] = useState(true);
+  const [isAdding, setIsAdding] = useState(false);
+
+  async function fetchProducts(curPage) {
+    try {
+      const payload = { params: { limit: 10, page: curPage } };
+      const { data } = await axios.get(`${apiURL}/products`, payload);
+      return data;
+    } catch (error) {
+      throw error.response.data.error;
+    }
+  }
 
   useEffect(() => {
-    async function fetchProducts() {
+    async function getProducts() {
       try {
-        console.log(`${apiURL}/products`);
-        const payload = { params: { limit: 10 } };
-        const { data } = await axios.get(`${apiURL}/products`, payload);
-        setProducts(data.products);
+        const data = await fetchProducts(page);
+        const total = data.totalProducts;
+        const newProducts = [...products, ...data.products];
+        setProducts(newProducts);
         setLoading(false);
+        const isLoadMore = total <= newProducts.length;
+        if (isLoadMore) setHasLoadMore(false);
+        setIsAdding(false);
       } catch (error) {
-        console.log(error.response);
+        console.log(error);
       } finally {
         setLoading(false);
       }
     }
-    fetchProducts();
-  }, []);
+    if (hasLoadMore) getProducts();
+  }, [page]);
+
+  const isCloseToBottom = ({
+    layoutMeasurement,
+    contentOffset,
+    contentSize
+  }) => {
+    const paddingToBottom = 20;
+    return (
+      layoutMeasurement.height + contentOffset.y >=
+      contentSize.height - paddingToBottom
+    );
+  };
+
+  const loadMore = () => {
+    if (hasLoadMore) {
+      setPage(page => page + 1);
+      setIsAdding(true);
+    }
+  };
 
   return (
-    <ScrollView>
+    <ScrollView
+      onScroll={({ nativeEvent }) => {
+        if (isCloseToBottom(nativeEvent)) {
+          loadMore();
+        }
+      }}
+    >
       <View style={styles.container}>
         <Text style={styles.heading}> Shop Categories </Text>
         <Categories />
         <Text style={styles.heading}> Product Overview </Text>
         {loading ? <SkeletonGrid /> : <ProductList products={products} />}
+        {isAdding && (
+          <View style={styles.loading}>
+            <ActivityIndicator color={colors.primary} size={35} />
+          </View>
+        )}
       </View>
     </ScrollView>
   );
@@ -55,5 +102,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '700',
     textTransform: 'uppercase'
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
